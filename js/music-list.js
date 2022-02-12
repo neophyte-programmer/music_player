eb9262c40aeb08059ef57443db1afc282e221722
Nutifafa Attor <attorfafa@gmail.com>
2022-02-12 11:11:00 +0000

    created array to store music

--- js/music-list.js
+++ js/music-list.js
@@ -1,9 +1,87 @@
 // Creating an array to store music
 let allMusic = [
-    {
-        name: "4:44",
-        artist: "JAY Z",
-        img: "",
-        src: ""
-    }
+	{
+		name: '4:44',
+		artist: 'JAY Z',
+		img: '444',
+		src: '444',
+	},
+	{
+		name: 'Aben Wo Ha',
+		artist: 'Daddy Lumba',
+		img: 'aben_wo_ha',
+		src: 'aben_wo_ha',
+	},
+	{
+		name: 'Blinding Lights',
+		artist: 'The Weeknd',
+		img: 'blinding_lights',
+		src: 'blinding_lights',
+	},
+	{
+		name: 'Fall In Love',
+		artist: 'D\'banj',
+		img: 'fall_in_love',
+		src: 'fall_in_love',
+	},
+	{
+		name: 'For Tonight',
+		artist: 'Giveon',
+		img: 'for_tonight',
+		src: 'for_tonight',
+	},
+	{
+		name: 'Get Down On It',
+		artist: 'Kool and the Gang',
+		img: 'get_down_on_it',
+		src: 'get_down_on_it',
+	},
+	{
+		name: 'Heat Waves',
+		artist: 'Glass Animals',
+		img: 'heat_waves',
+		src: 'heat_waves',
+	},
+	{
+		name: 'Let\'s Get It On',
+		artist: 'Marvin Gaye',
+		img: 'lets_get_it_on',
+		src: 'lets_get_it_on',
+	},
+	{
+		name: 'No Wahala',
+		artist: '1da Banton',
+		img: 'no_wahala',
+		src: 'no_wahala',
+	},
+	{
+		name: 'Peru',
+		artist: 'Fireboy DML ft Ed Sheeran',
+		img: 'peru',
+		src: 'peru',
+	},
+	{
+		name: 'Save Your Tears',
+		artist: 'The Weeknd ft Ariana Grande',
+		img: 'save_your_tears',
+		src: 'save_your_tears',
+	},
+	{
+		name: 'Stay',
+		artist: 'The Kid LAROI ft Justin Beiber',
+		img: 'stay',
+		src: 'stay',
+	},
+	{
+		name: 'Three Little Birds',
+		artist: 'Bob Marley and The Wailers',
+		img: 'three_little_birds',
+		src: 'three_little_birds',
+	},
+	{
+		name: 'Vibration',
+		artist: 'Fireboy DML',
+		img: 'vibration',
+		src: 'vibration',
+	},
 ]
